538c27fefc2530b8f18defb688f24914cf6892b8
VicroriaS <120009279+VicroriaS@users.noreply.github.com>
2023-05-14 15:11:13 +0300

    Updates search engine

---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div className="footer text-secondary mt-3">
+        <small>
+          <a
+            href="https://github.com/VicroriaS/weather-react"
+            target="_blank"
+            rel="noreferrer"
+            className="text-decoration-none"
+          >
+            Open source project
+          </a>
+          <span> by Viktoriia Sandul </span>
+        </small>
+      </div>
+    </div>
+  );
+}
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="Header">
+<h1> Forecast app</h1>
+      </div>
+
+  );
+}
